01ff737f1eb5f8dfe5cfff290303a69b37261432
icayeli <62623139+icayeli@users.noreply.github.com>
2022-12-08 10:25:32 +0800

    Add files via upload

--- insert.php
+++ insert.php
@@ -1,15 +1,24 @@
-<HTML>
-<body>
+
+
 <?php
 //Initialize variables
+session_start();
+use LDAP\Result;
+
 $servername = "127.0.0.1"; //default servername for phpmyadmin can also be 'localhost'
 $username = "root"; // default username
 $password = ""; //password used during installation of xampp
 $dbname = "capstone_database"; //Created database in phpmyadmin
 $tablename = "customers"; //Created table in phpmyadmin
-$firstname = $_POST["first_name"]; //input from MLTSampleDB.php
-$lastname = $_POST["last_name"]; //input from MLTSampleDB.php
-
+$firstname = $_POST["First_Name"]; //input from MLTSampleDB.php
+$lastname = $_POST["Last_Name"]; //input from MLTSampleDB.php
+$mobile = $_POST["Mobile_Number"];
+$email = $_POST["Email"];
+$baranggay = $_POST["Baranggay"];
+$province = $_POST["Province"];
+$city = $_POST["City"];
+$budget = $_POST["Budget"];
+$powerconsumption = $_POST["Power_Consumption"];
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -23,19 +32,26 @@ if ($conn->connect_error)
 //Create sql command for insert
 //SQL/MYSQL: Insert into studentTable(Surname,GivenName,MiddleName) Values('1','2','3');
 //C++: cout << "Insert into " + studentTable + "(Surname,GivenName,MiddleName)" + "Values('" + surname + "','" + givenname + "','" + middlename + "');";
-$sqlInsert = "Insert into " . $tablename . "(first_name,last_name)" . "Values('".$firstname."','".$lastname."');";
+$sqlInsert = "Insert into " . $tablename . "(First_Name,Last_Name,Mobile_Number,Email,Province,City,Baranggay,Budget,Power_Consumption)" . "Values('".$firstname."','".$lastname."','".$mobile."','".$email."','".$province."','".$city."','".$baranggay."','".$budget."','".$powerconsumption."');";
+
+
+
 
 //Condition and execute query
-if ($conn->query($sqlInsert) === TRUE) 
+if ($conn->query($sqlInsert) === TRUE)
 {
-  echo "New record created successfully";
-} 
+  $id = mysqli_insert_id($conn);
+  $_SESSION["id"]=$id;
+  header("Location: quote-details.php");
 
-else 	
-{
+}
+
+else {
   echo "Error: " . $sqlInsert . "<br>" . $conn->error;
 }
 ?>
-Welcome <?php echo $_POST["first_name"];?>!
-</body>
-</HTML>
+
+
+Welcome <?php echo $_POST["First_Name"];
+              
+?>!
